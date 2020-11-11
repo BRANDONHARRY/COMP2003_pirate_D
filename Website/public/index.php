c61f84eb09c0eaa8c028a0e0c49006babbb682e9
Brandon Kent <brandom.kent-2@students.plymouth.ac.uk>
2020-11-11 16:12:14 +0000

    all websites added, with hrefs to each other

--- Website/public/index.php
+++ Website/public/index.php
@@ -1,25 +1,27 @@
 <?php
-echo "test"
 ?>
 
 <head>
-    <title>Pirates Treasure!</title>
+    <title>Pirates Treasure Home</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="/pirate_D/Website/assets/css/css.css">
 </head>
+
 <body>
 
 <div class="header">
-    <h1>My Website</h1>
-    <p>A website created by me.</p>
+    <h1>Pirate Treasure!</h1>
+    <p>A website created by group_D</p>
 </div>
 
 <div class="navbar">
-    <a href="#">Link</a>
-    <a href="#">Link</a>
-    <a href="#">Link</a>
-    <a href="#" class="right">Link</a>
+    <a href="/pirate_D/Website/public/index.php" class="selected">Home</a>
+    <a href="/pirate_D/Website/public/learderboard.php">Leaderboards</a>
+    <a href="/pirate_D/Website/public/news.php">News</a>
+    <a href="/pirate_D/Website/public/download.php">Download</a>
+    <a href="/pirate_D/Website/public/about_us.php" class="right">About us</a>
+    <a href="/pirate_D/Website/public/contact_us.php" class="right">Contact us</a>
 </div>
 
 <div class="row">
---
+++ Website/public/news.php
@@ -0,0 +1,25 @@
+<?php
+?>
+
+<head>
+    <title>Pirates Treasure News</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/pirate_D/Website/assets/css/css.css">
+</head>
+
+<body>
+
+<div class="header">
+    <h1>Pirate Treasure!</h1>
+    <p>A website created by group_D</p>
+</div>
+
+<div class="navbar">
+    <a href="/pirate_D/Website/public/index.php">Home</a>
+    <a href="/pirate_D/Website/public/learderboard.php">Leaderboards</a>
+    <a href="/pirate_D/Website/public/news.php" class="selected">News</a>
+    <a href="/pirate_D/Website/public/download.php">Download</a>
+    <a href="/pirate_D/Website/public/about_us.php" class="right">About us</a>
+    <a href="/pirate_D/Website/public/contact_us.php" class="right">Contact us</a>
+</div>
