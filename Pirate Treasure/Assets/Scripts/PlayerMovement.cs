9200177d0c29fc28bd121e58f810fd6cc9a069b3
Hambush2 <55706777+Hambush2@users.noreply.github.com>
2020-11-17 11:34:00 +0000

    Player Boat Sprite added - Player Boat Movement Added
    Added sprite for the player, as well as providing movement, including stages of speed (-1, -0.5, 0, 0.5, 1) and turning.

---
+++ Pirate Treasure/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5.0f;
+    Rigidbody2D rigidbody2D;
+
+    float x = 0.0f;
+    public float y = 0.0f;
+    bool cooloff = false;
+    int waitFrames = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //This allows for the 'stages' of speed to function, rather than just jumping to the maximum value.
+        if (cooloff = true)
+        {
+            waitFrames--;
+            if (waitFrames < 0) 
+            {
+                cooloff = false;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.W) == true && cooloff == false)
+        {
+            y = y + 0.5f;
+
+            cooloff = true;
+            waitFrames = 15;
+            
+
+        }
+        else if (Input.GetKey(KeyCode.S) == true && cooloff == false)
+        {
+            y = y - 0.5f;
+
+            cooloff = true;
+            waitFrames = 15;
+
+        }
+
+
+        if(y > 1)
+        {
+            y = 1;
+        }
+        else if (y < -1)
+        {
+            y = -1;
+        }
+
+        rigidbody2D.velocity = transform.up * (y*speed);
+        rigidbody2D.angularVelocity = 0.0f;
+    }
+}
---
+++ Pirate Treasure/Assets/Scripts/PlayerTurning.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTurning : MonoBehaviour
+{
+    Rigidbody2D rigidbody2D;
+
+    public float smoothing = 5.0f;
+    //float x = 0.0f;
+    //float y = 0.0f;
+    //public float adjustmentAngle = 270.0f;
+
+    public float turnSpeed = 50.0f;
+
+    //float angle = 360.0f;
+    //float time = 1.0f;
+    //Vector3 axis = Vector3.forward;
+    //Vector3 backaxis = Vector3.back;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.D) == true)
+
+        {
+            //x = 1;
+            //float rotZ = Mathf.Atan2(y, x) * Mathf.Rad2Deg;
+            //Quaternion newRotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, rotZ + adjustmentAngle));
+            //transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.deltaTime * smoothing);
+
+            rigidbody2D.MoveRotation(rigidbody2D.rotation + -turnSpeed * Time.fixedDeltaTime);
+        }
+        else if (Input.GetKey(KeyCode.A) == true)
+        {
+            //x = -1;
+            //float rotZ = Mathf.Atan2(y, x) * Mathf.Rad2Deg;
+            //Quaternion newRotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, rotZ + adjustmentAngle));
+            //transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.deltaTime * smoothing);
+
+            rigidbody2D.MoveRotation(rigidbody2D.rotation + turnSpeed * Time.fixedDeltaTime);
+        }
+
+        //if (Input.GetKey(KeyCode.D) == false)
+        //{
+            //x = 0;
+        //}
+        //if (Input.GetKey(KeyCode.A) == false)
+        //{
+            //x = 0;
+        //}
+
+    }
+}
