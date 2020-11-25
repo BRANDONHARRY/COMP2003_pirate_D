7b8a3a13418eed23a20156d58553a77ffbd0da86
michalmpeis <58303680+michalmpeis@users.noreply.github.com>
2020-11-25 17:56:13 +0200

    tidying up hrefs

--- Website/public/index.php
+++ Website/public/index.php
@@ -5,27 +5,29 @@
     <title>Pirates Treasure Home</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/pirate_D/Website/assets/css/css.css">
+    <link rel="stylesheet" href="/COMP2003_pirate_D/Website/assets/css/css.css">
 </head>
 
 <body>
 
 <div class="header">
-    <img src="/pirate_D/Website/assets/img/pirate_treasure.png">
+    <img src="/COMP2003_pirate_D/Website/assets/img/pirate_treasure.png">
     <p>A website created by group_D</p>
 </div>
 
 <div class="navbar">
-    <a href="/pirate_D/Website/public/index.php" class="selected">Home</a>
-    <a href="/pirate_D/Website/public/learderboard.php">Leaderboards</a>
-    <a href="/pirate_D/Website/public/news.php">News</a>
-    <a href="/pirate_D/Website/public/download.php">Download</a>
-    <a href="/pirate_D/Website/public/about_us.php" class="right">About us</a>
-    <a href="/pirate_D/Website/public/contact_us.php" class="right">Contact us</a>
+    <a href="/COMP2003_pirate_D/Website/public/index.php" class="selected">Home</a>
+    <a href="/COMP2003_pirate_D/Website/public/learderboard.php">Leaderboards</a>
+    <a href="/COMP2003_pirate_D/Website/public/news.php">News</a>
+    <a href="/COMP2003_pirate_D/Website/public/download.php">Download</a>
+    <a href="/COMP2003_pirate_D/Website/public/about_us.php" class="right">About us</a>
+    <a href="/COMP2003_pirate_D/Website/public/contact_us.php" class="right">Contact us</a>
 </div>
 
 <div class="row">
-
+    <div class="main">
+        <h1></h1>
+    </div>
 </div>
 
 <div class="footer">
--- Website/public/news.php
+++ Website/public/news.php
@@ -5,7 +5,7 @@
     <title>Pirates Treasure News</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/pirate_D/Website/assets/css/css.css">
+    <link rel="stylesheet" href="/COMP2003_pirate_D/Website/assets/css/css.css">
 </head>
 
 <body>
@@ -16,10 +16,10 @@
 </div>
 
 <div class="navbar">
-    <a href="/pirate_D/Website/public/index.php">Home</a>
-    <a href="/pirate_D/Website/public/learderboard.php">Leaderboards</a>
-    <a href="/pirate_D/Website/public/news.php" class="selected">News</a>
-    <a href="/pirate_D/Website/public/download.php">Download</a>
-    <a href="/pirate_D/Website/public/about_us.php" class="right">About us</a>
-    <a href="/pirate_D/Website/public/contact_us.php" class="right">Contact us</a>
+    <a href="/COMP2003_pirate_D/Website/public/index.php">Home</a>
+    <a href="/COMP2003_pirate_D/Website/public/learderboard.php">Leaderboards</a>
+    <a href="/COMP2003_pirate_D/Website/public/news.php" class="selected">News</a>
+    <a href="/COMP2003_pirate_D/Website/public/download.php">Download</a>
+    <a href="/COMP2003_pirate_D/Website/public/about_us.php" class="right">About us</a>
+    <a href="/COMP2003_pirate_D/Website/public/contact_us.php" class="right">Contact us</a>
 </div>
