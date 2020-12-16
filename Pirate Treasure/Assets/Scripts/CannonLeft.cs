0fd1927b49569dedde3d4dd4c934985dc9b9d366
Hambush2 <55706777+Hambush2@users.noreply.github.com>
2020-12-16 10:12:17 +0000

    Added firing to Player
    Player can now fire cannonball's at enemies that will damage and kill them, scoring the player points

---
+++ Pirate Treasure/Assets/Scripts/CannonLeft.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonLeft : MonoBehaviour
+{
+    public GameObject CannonballPrefab;
+    public Transform CannonballSpawn;
+    public float fireTime = 0.5f;
+
+    private bool isFiring = false;
+
+    private void SetFiring()
+    {
+        isFiring = false;
+    }
+
+    private void Fire()
+    {
+        isFiring = true;
+        Instantiate(CannonballPrefab, CannonballSpawn.position, CannonballSpawn.rotation);
+
+        if (GetComponent<AudioSource>() != null)
+        {
+            GetComponent<AudioSource>().Play();
+        }
+
+        Invoke("SetFiring", fireTime);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("q"))
+        {
+            if (!isFiring)
+            {
+                Fire();
+            }
+        }
+    }
+}
---
+++ Pirate Treasure/Assets/Scripts/CannonballLeft.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonballLeft: MonoBehaviour
+{
+    public float movespeed = 10.0f;
+    public int damage = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().AddForce(-transform.right * movespeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        other.transform.SendMessage("TakeDamage", damage, SendMessageOptions.DontRequireReceiver);
+        Die();
+    }
+
+    private void OnBecameInvisible()
+    {
+        Die();
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
