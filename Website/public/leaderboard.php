dc1fe6242c02986c8054c0ebc32c50e10bc96d3d
Brandon Kent <brandom.kent-2@students.plymouth.ac.uk>
2021-02-03 14:49:08 +0000

    Change layout of webpages

--- Website/public/leaderboard.php
+++ Website/public/leaderboard.php
@@ -2,24 +2,71 @@
 ?>
 
 <head>
-    <title>Pirates Treasure Leaderboard</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/COMP2003_pirate_D/Website/assets/css/css.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Pirates Treasure Contact</title>
+
+    <style>
+        .header {
+            padding: 80px;
+            text-align: center;
+            color: white;
+            background-size:100% 100%;
+            background-image: url("../assets/img/Game.png");
+        }
+        .footer {
+            position: absolute;
+            bottom: 0;
+            width: 100%;
+            height: 60px;
+            line-height: 60px;
+            background-color: #343a40;
+        }
+    </style>
 </head>
 
 <body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <a class="navbar-brand" href="index.php"><img src="../assets/img/pirate.png" class="d-inline-block align-top"
+                                                      width="77.5" height="56"></a>
+
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Home </a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="leaderboard.php">Leaderboards</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="news.php">News</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="download.php">Download</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="about_us.php">About us</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="contact_us.php">Contact us</a>
+            </li>
+        </ul>
+    </div>
+</nav>
 
 <div class="header">
     <img src="/COMP2003_pirate_D/Website/assets/img/pirate_treasure.png">
     <p>A website created by group_D</p>
 </div>
 
-<div class="navbar">
-    <a href="/COMP2003_pirate_D/Website/public/index.php">Home</a>
-    <a href="/COMP2003_pirate_D/Website/public/leaderboard.php" class="selected">Leaderboards</a>
-    <a href="/COMP2003_pirate_D/Website/public/news.php">News</a>
-    <a href="/COMP2003_pirate_D/Website/public/download.php">Download</a>
-    <a href="/COMP2003_pirate_D/Website/public/about_us.php" class="right">About us</a>
-    <a href="/COMP2003_pirate_D/Website/public/contact_us.php" class="right">Contact us</a>
-</div>
+<footer class="footer">
+    <div class="container">
+            <span class="text-muted">Created by COMP2003 Group_D. Link to Github:
+            <a href="https://github.com/BRANDONHARRY/COMP2003_pirate_D">https://github.com/BRANDONHARRY/COMP2003_pirate_D</a></span>
+    </div>
+</footer>
